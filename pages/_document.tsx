d40258ea0841f42e7eb273d254c357c2a517b3b4
kmvandenboss <kevin@vandenbossgroup.com>
2024-12-03 10:31:00 -0500

    GTM setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PNM9N93P');
+          `}
+        </Script>
+
         {/* Meta Pixel Code */}
         <Script id="fb-pixel" strategy="afterInteractive">
           {`
@@ -25,12 +36,20 @@ export default function Document() {
             height="1" 
             width="1" 
             style={{ display: 'none' }}
-            src={`https://www.facebook.com/tr?id=1870309123496957&ev=PageView&noscript=1`}
+            src="https://www.facebook.com/tr?id=1870309123496957&ev=PageView&noscript=1"
           />
         </noscript>
-        {/* End Meta Pixel Code */}
       </Head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PNM9N93P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
